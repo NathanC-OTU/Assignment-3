563ef02cc69f575b35cb4a891201eed937580f86
NathanC-OTU <114961208+NathanC-OTU@users.noreply.github.com>
2022-11-16 16:20:25 -0500

    File structure, empty content

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-const http = require('node:http');
+var connect = require('connect')
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+var app = connect
